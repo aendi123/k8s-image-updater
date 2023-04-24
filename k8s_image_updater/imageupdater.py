ffceced8b1689aa98d06f89bb70506a2167e8c87
Andrin Kessler <andrin.kessler@ik.me>
2023-04-24 22:35:09 +0200

    add method to list all supported k8s manifests

--- k8s_image_updater/imageupdater.py
+++ k8s_image_updater/imageupdater.py
@@ -1,6 +1,9 @@
-import argparse
 from pathlib import Path
+import argparse
 import re
+import yaml
+
+SUPPORTED_K8S_TYPES='Deployment|DaemonSet|StatefulSet'
 
 def run(args):
     path = Path(args.path)
@@ -10,9 +13,11 @@ def run(args):
         all_yaml_files = list_all_yaml_files(path, args.exclude)
     else:
         all_yaml_files = list_all_yaml_files(path)
-    
-    for file in all_yaml_files:
-        print(file)
+
+    supported_yaml_files = list_supported_yaml_files(all_yaml_files)
+
+    for yaml_file in supported_yaml_files:
+        print(yaml_file.name)
 
 
 def list_all_yaml_files(path, exclude=''):
@@ -34,6 +39,22 @@ def list_all_yaml_files(path, exclude=''):
             all_yaml_files.append(file)
 
     return all_yaml_files
+
+
+def list_supported_yaml_files(yaml_files):
+    supported_yaml_files = []
+
+    for yaml_file in yaml_files:
+        with open(yaml_file, 'r') as yaml_file:
+            all_yamls = yaml.safe_load_all(yaml_file)
+            for yaml_data in all_yamls:
+                try: 
+                    if re.match(SUPPORTED_K8S_TYPES, yaml_data['kind']):
+                        supported_yaml_files.append(yaml_file)
+                except (KeyError, TypeError):
+                    pass
+    
+    return supported_yaml_files
 
 
 def get_parser():
